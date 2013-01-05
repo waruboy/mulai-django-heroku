b9d347c57fdf757149206c773fb1a6a37f3c44ab
Taufiq Muhammadi <waruboy@yahoo.com>
2013-01-05 11:20:54 +0900

    static_url ganti

--- aplikasi/settings.py
+++ aplikasi/settings.py
@@ -66,7 +66,7 @@ STATIC_ROOT = os.path.join(PROJECT_DIR, 'static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = '/biliq/static/'
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
