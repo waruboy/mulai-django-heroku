d9ba233ceaf8936615eb6b31a60e935d1c3f7164
Taufiq Muhammadi <waruboy@yahoo.com>
2013-01-05 11:15:49 +0900

    seting untuk static_tambahan

--- aplikasi/settings.py
+++ aplikasi/settings.py
@@ -70,6 +70,7 @@ STATIC_URL = '/biliq/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
+    os.path.join(PROJECT_DIR, 'static_tambahan')
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
